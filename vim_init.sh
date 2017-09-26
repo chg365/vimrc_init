068e9b19c5150d730e916bd94179060e900a13a1
Cui Haiguang <cuihaiguang@eyou.net>
2017-09-26 19:36:38 +0800

    centos5 test

--- vim_init.sh
+++ vim_init.sh
@@ -1,5 +1,44 @@
 #!/bin/bash
 
+#################################################################################
+#                                    系统检测
+#################################################################################
+
+# {{{ function exists_soft()
+function exists_soft()
+{
+    local is_error="0"
+
+    for i in `echo "$@"`; do
+        which $i >/dev/null 2>&1
+        if [ "$?" != "0" ];then
+            is_error="1"
+            echo "not find soft $i" >&2
+        fi
+    done
+
+    if [ "$is_error" = "1" ];then
+        exit 1;
+    fi
+}
+# }}}
+exists_soft git vim sort pwd ctags
+
+#sudo yum -y install ctags vim-minimal vim-common vim-enhanced
+
+# 检测sort 是否支持版本号排序
+sort_V="V"
+echo '' |sort -V >/dev/null 2>&1
+if [ "$?" != "0" ];then
+    #不支持
+    sort_V=""
+    echo "Warning: sort not has -V" >&2
+fi
+
+#################################################################################
+#                                    目录和变量定义
+#################################################################################
+
 curr_dir=`pwd`;
 
 VIM_TMP_DIR="/tmp/${LOGNAME}/vim"
@@ -9,11 +48,6 @@ if [ -d "$VIM_TMP_DIR" ];then
     exit 1;
 fi
 
-which vim >/dev/null 2>&1
-if [ "$?" != "0" ];then
-    sudo yum -y install ctags vim-minimal.x86_64 vim-common.x86_64 vim-enhanced.x86_64 &
-fi
-
 bundle_dir=$HOME/.vim/bundle
 colors_dir=$HOME/.vim/colors
 autoload_dir=$HOME/.vim/autoload
@@ -21,20 +55,48 @@ autoload_dir=$HOME/.vim/autoload
 mkdir -p $VIM_TMP_DIR
 mkdir -p $HOME/.vim/{bundle,colors,autoload}
 
+# {{{ function git_clone()
+function git_clone()
+{
+    local url="$1"
+    local pos="$2"
 
+    if [ "$url" = "" ];then
+        echo "git clone not find url" >&2
+        return 1;
+    fi
 
-###################################################################
-#                            Vundle
-###################################################################
+#if [ "$pos" != "" ];then
+#        git clone $url $pos  >/dev/null 2>&1
+#    else
+#        git clone $url  >/dev/null 2>&1
+#    fi
+    git clone $url $pos  >/dev/null 2>&1
 
-# git clone https://github.com/gmarik/Vundle.vim.git $bundle_dir/Vundle.vim &
+    if [ "$?" != "0" ];then
+        echo "git clone faild. url: $url  dir: $pos"
+            return 1;
+    fi
+}
+# }}}
+# {{{ function git_pull()
+function git_pull()
+{
+    git pull >/dev/null 2>&1
+
+    if [ "$?" != "0" ];then
+        echo "git pull faild. dir: `pwd`" >&2
+        return 1;
+    fi
+}
+# }}}
 
 ###################################################################
 #                            pathogen
 ###################################################################
 
 cd $VIM_TMP_DIR && \
-   git clone https://github.com/tpope/vim-pathogen.git && \
+   git_clone "https://github.com/tpope/vim-pathogen.git" && \
    cp vim-pathogen/autoload/pathogen.vim $autoload_dir/ &
 
 ###################################################################
@@ -42,11 +104,11 @@ cd $VIM_TMP_DIR && \
 ###################################################################
 
 cd $VIM_TMP_DIR && \
-   git clone https://github.com/tomasr/molokai.git && \
+   git_clone "https://github.com/tomasr/molokai.git" && \
    cp molokai/colors/molokai.vim $colors_dir/ &
 
 cd $VIM_TMP_DIR && \
-   git clone https://github.com/altercation/vim-colors-solarized.git && \
+   git_clone "https://github.com/altercation/vim-colors-solarized.git" && \
    cp vim-colors-solarized/colors/solarized.vim $colors_dir/ &
 
 ###################################################################
@@ -63,7 +125,7 @@ do
     cd $i 
     #git remote -v |head -1 | awk '{print $2;}'
     if [ -d ".git" ];then
-        git pull
+        git_pull
     fi
 } &
 done
@@ -71,7 +133,7 @@ done
 #nginx
 {
 cd $VIM_TMP_DIR && \
-   git clone https://github.com/nginx/nginx.git
+   git_clone "https://github.com/nginx/nginx.git"
 
 if [ "$?" = "0" ];then
    rm -rf $bundle_dir/nginx
@@ -83,45 +145,46 @@ fi
 bundle_arr=(
         "https://github.com/ctrlpvim/ctrlp.vim.git"
         "https://github.com/rizzatti/dash.vim.git"
-        "https://github.com/StanAngeloff/php.vim.git"
         "https://github.com/airblade/vim-gitgutter.git"
         "https://github.com/mattn/emmet-vim.git"
         "https://github.com/Yggdroot/indentLine.git"
-        # "https://github.com/Shougo/neocomplcache.vim.git"
-        # "https://github.com/Shougo/neocomplete.vim.git"
         "https://github.com/scrooloose/nerdcommenter.git"
-        "https://github.com/scrooloose/nerdtree.git"
         "https://github.com/msanders/snipmate.vim.git"
         "https://github.com/godlygeek/tabular.git"
-        "https://github.com/majutsushi/tagbar.git"
         "https://github.com/vim-scripts/taglist.vim.git"
-        "https://github.com/vim-syntastic/syntastic.git"
-        "https://github.com/vim-scripts/OmniCppComplete.git"
         "https://github.com/plasticboy/vim-markdown.git"
-        "https://github.com/ap/vim-css-color.git"
         "https://github.com/fatih/vim-go.git"
         "https://github.com/yianwillis/vimcdoc.git"
         "https://github.com/jelera/vim-javascript-syntax.git"
-        "https://github.com/gmarik/Vundle.vim.git"
         "https://github.com/shawncplus/phpcomplete.vim.git"
         "https://github.com/ervandew/supertab.git"
         "https://github.com/Lokaltog/vim-powerline.git"
+        #"https://github.com/vim-scripts/OmniCppComplete.git"
+        # "https://github.com/gmarik/Vundle.vim.git"
         ##"https://github.com/powerline/powerline.git"
 );
 
+version=`vim --version|head -1|awk '{print $5;}'`;
 use_neo=0;
 if vim --version|grep -q '+lua' ;then
-    version=`vim --version|head -1|awk '{print $5;}'`;
-    ver=`echo "$version 7.4"|tr ' ' '\n'|sort -rV|head -1`
+    ver=`echo "$version 7.4"|tr ' ' '\n'|sort -r${sort_V}|head -1`
     if [ "$ver" = "$version" ];then
         use_neo=1;
     fi
 fi
 if [ "$use_neo" = "0" ];then
     bundle_arr[${#bundle_arr[@]}]="https://github.com/Shougo/neocomplcache.vim.git"
-else
+fi
+
+ver=`echo "$version 7.1"|tr ' ' '\n'|sort -r${sort_V}|head -1`
+if [ "$ver" = "$version" ];then
     # 使用neocomplete.vim
     bundle_arr[${#bundle_arr[@]}]="https://github.com/Shougo/neocomplete.vim.git"
+    bundle_arr[${#bundle_arr[@]}]="https://github.com/vim-syntastic/syntastic.git"
+    bundle_arr[${#bundle_arr[@]}]="https://github.com/majutsushi/tagbar.git"
+    bundle_arr[${#bundle_arr[@]}]="https://github.com/scrooloose/nerdtree.git"
+    bundle_arr[${#bundle_arr[@]}]="https://github.com/ap/vim-css-color.git"
+    bundle_arr[${#bundle_arr[@]}]="https://github.com/StanAngeloff/php.vim.git"
 fi
 
 for url in "${bundle_arr[@]}";do
@@ -129,7 +192,7 @@ for url in "${bundle_arr[@]}";do
     dir_name=${url##*/}
     dir_name=${dir_name%%.git}
     if [ ! -d "$bundle_dir/$dir_name" ];then
-        git clone $url $bundle_dir/$dir_name &
+        git_clone "$url" "$bundle_dir/$dir_name" &
     fi
 done
 
